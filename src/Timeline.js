4eba49677faa04d7ca5f2b3e141a601909272e2c
rsbisne <roshansb99@gmail.com>
2023-07-23 14:50:51 +0530

    created the timeline

---
+++ src/Timeline.js
@@ -0,0 +1,71 @@
+import React from "react";
+import "./index.css";
+export default function Timeline() {
+  return (
+    <div>
+      <div className="entries">
+        <div className="entry">
+          <div className="title">2015</div>
+          <div className="body">
+            <p>SSC done.</p>
+          </div>
+        </div>
+        <div className="entry">
+          <div className="title">2017</div>
+          <div className="body">
+            <p>HSC done.</p>
+          </div>
+        </div>
+        <div className="entry">
+          <div className="title big">2018</div>
+          <div className="body">
+            <p>First year.</p>
+          </div>
+        </div>
+        <div className="entry">
+          <div className="title">2019</div>
+          <div className="body">
+            <p>Second year</p>
+          </div>
+        </div>
+        <div className="entry">
+          <div className="title">2020</div>
+          <div className="body">
+            <p>Pandemic.</p>
+            <p>Third year.</p>
+          </div>
+        </div>
+        <div className="entry">
+          <div className="title big">2021</div>
+          <div className="body">
+            <p>Fourth year.</p>
+          </div>
+        </div>
+        <div className="entry">
+          <div className="title">2022</div>
+          <div className="body">
+            <p>Fifth year.</p>
+          </div>
+        </div>
+        <div className="entry">
+          <div className="title">2023</div>
+          <div className="body">
+            <p>Intern.</p>
+          </div>
+        </div>
+        <div className="entry">
+          <div className="title">2024</div>
+          <div className="body">
+            <p>Level up.</p>
+          </div>
+        </div>
+        <div className="entry">
+          <div className="title">2025</div>
+          <div className="body">
+            <p>Ready.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
